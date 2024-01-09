84cdd9ae3c1b2cbdd873cdb47b73ef4c0078038d
Ilkka Peltola <ilkka.peltola@zervant.com>
2024-01-09 16:50:29 +0200

    storage

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 import { getFingerprint, getFingerprintData } from './fingerprint/functions';
 import * as packageJson from '../package.json';
 import './components'
+import { getItem, setItem } from './storage'
 
 //const componentsContext = require.context('./components', true, /^(?!.*\.test\.ts$).*\.ts$/);
 //componentsContext.keys().forEach(componentsContext);
 
-interface fingerprintOptionsInterface {
-    showElapsed?: boolean
+export interface fingerprintOptionsInterface {
+    showElapsed: boolean,
+    storageUrl: string,
+    namespace: string
 }
 
 const options: fingerprintOptionsInterface = {
-    showElapsed: false
+    showElapsed: false,
+    storageUrl: 'https://storage-test.thumbmarkjs.com/v1/fingerprint',
+    namespace: 'thumbmarkjs'
 }
 
 function getVersion(): string {
     return packageJson.version;
 }
 
-export { getVersion, getFingerprint, getFingerprintData, options }
+export { getVersion, getFingerprint, getFingerprintData, getItem, setItem, options }
---
+++ src/storage/index.ts
@@ -0,0 +1,5 @@
+import { getItem } from './getItem'
+import { setItem } from './setItem'
+
+export { setItem, getItem }
+
